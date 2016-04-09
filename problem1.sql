f6561c27d1b043c261bc53d0a0fca75b3a26b574
ClimbsRocks <climbsbytes@gmail.com>
2016-04-08 21:21:01 -0700

    makes date format consistent with example

--- problem1.sql
+++ problem1.sql
@@ -1,9 +1,11 @@
+-- calculate monthly active users for October
 WITH mau AS (
   SELECT COUNT(DISTINCT(owner)) AS mau
   FROM activity_data
   WHERE extract(month from act_time_local) = 10
 )
-SELECT DATE_TRUNC('day', act_time_local) AS local_date, COUNT(DISTINCT(owner)) AS dau, mau.mau
+
+SELECT DATE_TRUNC('day', act_time_local)::date AS local_date, COUNT(DISTINCT(owner)) AS dau, mau.mau
 FROM activity_data, mau
 WHERE extract(month from act_time_local) = 10
 GROUP BY local_date, mau.mau
