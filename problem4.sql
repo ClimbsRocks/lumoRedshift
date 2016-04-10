7d2e38035cf15ce066c3fa27798996e583d71c85
ClimbsRocks <climbsbytes@gmail.com>
2016-04-09 17:22:03 -0700

    lays out structure for the rest of the CTEs

--- problem4.sql
+++ problem4.sql
@@ -25,16 +25,26 @@ WITH positive_days AS (
   HAVING SUM(step_count_chunks.steps) >= 500 OR SUM(good_posture_chunks.good_posture_time) >= 30
   ORDER BY owner, local_date
 ), 
+-- calculate which "streak_group" each day falls into for each owner
+streak_groups_by_day AS (
+  -- if we subtract the date the Lumo Lift launched from the date of the activity, we get an integer
+  -- each date that is in the same streak will have this same integer, plus the number of rows that date is within the streak
+  -- so if we partition by the owner, and then order by the date, the row number within that partition can be used to calculate which "streak_group" each activity falls into
+  SELECT owner, local_date, local_date - '2014-01-01'::date - row_number() OVER (PARTITION BY owner ORDER BY local_date) AS streak_group
+  FROM positive_days
+), 
 
+-- calculate the length of each streak, grouped by user
+streak_lengths_by_user AS (
+  SELECT owner, streak_group, COUNT(streak_group) AS streak_length
+  FROM streak_groups_by_day
+  GROUP BY owner, streak_group
+  HAVING COUNT(streak_group) > 1
+  ORDER BY owner
+)
 
+-- finish by simply taking the count of streaks, and the max length of a streak
 
-
-
-
-
-
-
-
-
-
-
+SELECT owner, COUNT(*) AS n_streaks, MAX(streak_length) AS longest_streak
+FROM streak_lengths_by_user
+GROUP BY owner;
